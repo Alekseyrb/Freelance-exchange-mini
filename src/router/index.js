defcadd0771093a2b490bb47b1307c722637e323
Oleksii <kravchenko.a.a.rb@gmail.com>
2023-04-20 12:50:01 +0300

    add not found page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from "vue-router";
 import Tasks from "@/views/Tasks.vue";
 import Task from "@/views/Task.vue";
 import New from "@/views/New.vue";
+import notFound from "@/views/NotFound.vue";
 
 export const router = createRouter({
         history: createWebHistory(),
@@ -9,6 +10,7 @@ export const router = createRouter({
             {path: '/tasks', component: Tasks, alias: '/'},
             {path: '/create', component: New},
             {path: '/task/:taskId?', component: Task},
+            {path: '/:notFound(.*)', component: notFound},
         ],
         linkActiveClass: 'active',
         linkExactActiveClass: 'active',
---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="card">
+    <h1>Страница не найдена</h1>
+    <p>
+      Такого пути не существует {{ $route.fullPath }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+}
+</script>
